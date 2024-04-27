1f74d02fc7eb2cefc5a37d7f18f475e3c8504e19
Giordano94 <giordano.mendes94@gmail.com>
2024-04-27 15:22:57 -0300

    feat(data): Add projects Mock

---
+++ src/Data/projects.js
@@ -0,0 +1,31 @@
+export const projectsData = [
+  {
+    id: 1,
+    image: 'url',
+    title: 'Star Wars',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam sapiente similique mollitia error officia incidunt dolore dolor nesciunt tempora fugit rem hic molestiae est sunt aperiam suscipit nulla, accusamus deserunt',
+    link: 'link do github',
+  },
+  {
+    id: 2,
+    image: 'url',
+    title: 'title project',
+    description: 'desciption',
+    link: 'pages/',
+  },
+  {
+    id: 3,
+    image: 'url',
+    title: 'title project',
+    description: 'desciption',
+    link: 'pages/',
+  },
+  {
+    id: 4,
+    image: 'url',
+    title: 'title project',
+    description: 'desciption',
+    link: 'pages/',
+  },
+];
