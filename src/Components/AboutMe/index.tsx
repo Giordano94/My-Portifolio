404451cb0357f6521c983fc274db2227045e814f
Giordano94 <giordano.mendes94@gmail.com>
2024-04-29 21:14:20 -0300

    feat(AboutMe): Create AboutMe component

---
+++ src/Components/AboutMe/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+const AboutMe: FC = () => {
+  return (
+    <div>
+      <h1>AboutMe</h1>
+      <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+        necessitatibus dolorum, sapiente accusamus tempore accusantium quas
+        eligendi. Magni cum sunt ipsam impedit quo! Ipsa quas optio officiis nam
+        cumque temporibus!
+      </section>
+    </div>
+  );
+};
+
+export default AboutMe;
