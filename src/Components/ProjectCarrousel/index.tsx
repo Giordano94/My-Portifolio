85554301637c7087094d58fa25802719c6dbc63e
Giordano94 <giordano.mendes94@gmail.com>
2024-06-14 11:54:17 -0300

    feat(ProjectCarousel): create ProjectCarousel component

---
+++ src/Components/ProjectCarrousel/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface ProjectCarouselProps {}
+
+const ProjectCarousel: FC<ProjectCarouselProps> = () => {
+  return (
+    <div>
+      <h1>Carroussel here</h1>
+    </div>
+  );
+};
+
+export default ProjectCarousel;
