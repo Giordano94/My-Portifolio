b2332c628686306a625f2c2830a8848bd01b1aab
Giordano94 <giordano.mendes94@gmail.com>
2024-06-14 11:55:21 -0300

    feat: rename component and refactr Home

--- src/Pages/InitialPage/Index.tsx
+++ src/Pages/InitialPage/Index.tsx
@@ -6,20 +6,27 @@ import { projectsData } from '../../Data/projects';
 import Footer from '../../Components/Footer';
 import AboutMe from '../../Components/AboutMe';
 import IntroductionText from '../../Components/IntroductionText';
+import ProjectCarousel from '../../Components/ProjectCarrousel';
 
 export const Home: FC = () => {
-  const renderProjectCards = useMemo(() => {
-    return projectsData.map((project) => (
-      <ProjectCard key={project.id} project={project} />
-    ));
-  }, []);
+  {
+    /*
+    
+    const renderProjectCards = useMemo(() => {
+      return projectsData.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+        ));
+        }, [projectsData]);
+        */
+  }
 
   return (
     <Container>
       <Header />
       <IntroductionText />
       <AboutMe />
-      <div>{renderProjectCards}</div>
+      {/*<div>{renderProjectCards}</div>*/}
+      <ProjectCarousel />
       <Footer />
     </Container>
   );
